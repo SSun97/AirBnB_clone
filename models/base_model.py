cace611eec0e96afee7daadf0b07996fa2d1f786
Simon Sun <sunmingyu.97@gmail.com>
2021-05-18 18:35:15 -0500

    typo created_at

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
 
     def __init__(self):
-        self.create_at = datetime.now()
+        self.created_at = datetime.now()
         self.update_at = datetime.now()
         self.id = str(uuid.uuid4())
 
@@ -24,28 +24,12 @@ class BaseModel:
         instance_dict = self.__dict__.copy()
         instance_dict['__class__'] = str(type(self).__name__)
         for k in instance_dict.keys():
-            if k == "create_at":
+            if k == "created_at":
                 instance_dict[k] = instance_dict[k].isoformat()
             if k == "update_at":
                 instance_dict[k] = self.update_at.isoformat()
         return instance_dict
 
-
-
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
-
-
-
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
