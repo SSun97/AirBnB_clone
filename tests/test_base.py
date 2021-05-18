2ae5bca8408bbe578c5b46f9eb79a67f2382cf7b
Simon Sun <sunmingyu.97@gmail.com>
2021-05-18 18:58:11 -0500

    test save()

--- tests/test_base.py
+++ tests/test_base.py
@@ -13,11 +13,13 @@ class TestClassMerthods(unittest.TestCase):
     """test the Airbnb project"""
 
     def test_BaseModel_id(self):
-        self.assertEqual(type(BaseModel().genarate_id()), uuid.UUID)
+        self.assertEqual(type(BaseModel().id), str)
 
     def test_create_time(self):
-        self.assertEqual(type(BaseModel().create_at()), datetime)
+        self.assertEqual(type(BaseModel().created_at), datetime)
 
+    def test_save(self):
+        self.assertEqual(BaseModel().save(), None)
 
 
 
