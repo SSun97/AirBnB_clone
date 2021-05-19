941030fbd439fd96f0c3b7ca0b135985a8c55d31
Simon Sun <sunmingyu.97@gmail.com>
2021-05-19 17:40:15 +0000

    more test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -33,6 +33,9 @@ class TestClassMerthods(unittest.TestCase):
         nb2 = BaseModel(**dict1)
         dict2 = nb2.to_dict()
         self.assertEqual(dict1, dict2)
+        self.assertTrue(type(dict1) is dict)
+        self.assertEqual(dict1["__class__"], "BaseModel")
+        self.assertTrue(type(dict1["created_at"]) is str)
 
 
     def assertHasAttr(self):
