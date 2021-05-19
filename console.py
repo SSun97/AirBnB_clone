dc8520ff296d20c225cf622fb126d68956e6e0af
Simon Sun <sunmingyu.97@gmail.com>
2021-05-19 18:16:53 -0500

    first push on console

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """the console of airbnb"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg: str):
+        'Quit command to exit the program\n'
+        return True
+    def do_EOF(self, arg: str):
+        'Exit the program'
+        return True
+    def emptyline(self) -> bool:
+        False
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
