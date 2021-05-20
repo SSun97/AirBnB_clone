273aeb9efcd4ed846907964255d7ba6806ac0c47
Simon Sun <sunmingyu.97@gmail.com>
2021-05-20 13:50:35 -0500

    edit the logic of update function

--- tmp_console_main.py
+++ tmp_console_main.py
@@ -104,35 +104,38 @@ class HBNBCommand(cmd.Cmd):
             print(list_of_strings)
             return
 
-    def do_update(self, arg):
+    def do_update(self,arg):
         """update the information of an instance\n"""
         if not arg:
             print("** class name missing **")
             return
         args = arg.split()
-        if args[0] != 'BaseModel':
-            print("** class doesn't exist")
-            return
-        if len(args) == 1:
+        if len(args) < 1:
+            if args[0] != 'BaseModel':
+                print("** class doesn't exist")
+                return
+        elif len(args) < 2:
             print("** instance id missing **")
             return
-        if len(args) >= 2:
+        elif len(args) < 3:
             for key, val in models.storage.all().items():
                 if args[1] in key.split("."):
-                    if len(args) == 2:
-                        print(" ** attribute name missing **")
-                        return
-                    if len(args) == 3:
-                        print("** value missing **")
-                        return
-                    if len(args) >= 4:
-                        setattr(models.storage.all()[key], args[2], args[3])
-                        models.storage.save()
-                        models.storage.reload()
-                        return
-                else:
-                    print("** no instance found **")
+                    print("** attribute name missing **")
                     return
+            else:
+                print("** no instance found **")
+        elif len(args) < 4:
+            print("** value missing **")
+            return
+        else:
+            for key, val in models.storage.all().items():
+                if args[1] in key.split("."):
+                    setattr(models.storage.all()[key], args[2], args[3])
+                    models.storage.save()
+                    models.storage.reload()
+                    return
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
