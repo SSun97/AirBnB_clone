75668532b4eec7e308336100d31cf42b62e021d8
Simon Sun <sunmingyu.97@gmail.com>
2021-05-21 00:50:11 -0500

    unittest

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""test the airbnb project"""
+
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+import json
+from os import remove, path
+
+from models.user import User
+
+
+class TestClassMerthods(unittest.TestCase):
+    """test the Airbnb project"""
+
+    def test_user_email(self):
+        self.assertTrue(hasattr(User, 'email'))
+
+    def test_user_password(self):
+        self.assertTrue(hasattr(User, 'password'))
+
+    def test_user_first_name(self):
+        self.assertTrue(hasattr(User, 'first_name'))
+
+    def test_user_last_name(self):
+        self.assertTrue(hasattr(User, 'last_name'))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
