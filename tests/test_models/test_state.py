8a8e702fecde0e6e5e8012995e383dd11cc78138
Simon Sun <sunmingyu.97@gmail.com>
2021-05-21 19:58:06 -0500

    test class State

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""test the airbnb project"""
+
+
+import unittest
+from models.state import State
+
+A = State()
+class TestClassMerthods(unittest.TestCase):
+    """test the Airbnb project"""
+
+    def test_State_attr(self):
+        self.assertEqual(type(A.name), str)
