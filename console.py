334f0726de9a984657b03657ec51bdadd96fe554
Simon Sun <sunmingyu.97@gmail.com>
2021-05-23 12:33:55 -0500

    debug

--- console.py
+++ console.py
@@ -18,8 +18,8 @@ class HBNBCommand(cmd.Cmd):
     """the console of airbnb"""
     prompt = '(hbnb) '
 
-    def default(self, line: str):
-        args = line.split(".")
+    # def default(self, line: str):
+    #     args = line.split(".")
 
     def do_quit(self, arg: str):
         'Quit command to exit the program\n'
