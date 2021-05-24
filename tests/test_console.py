b717416c9b15e6668dd97aa291bfe14ec60ec4b2
Simon Sun <sunmingyu.97@gmail.com>
2021-05-24 00:13:50 -0500

    Create test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""test the airbnb project"""
+
+
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from console import HBNBCommand
+
+with patch('sys.stdout', new=StringIO()) as f:
+    string = HBNBCommand().onecmd("help show")
+
+class TestClassMethods(unittest.TestCase):
+    """test the Airbnb project"""
+
+    def test_help(self):
+        """Tests the help command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help quit")
+        string = """Quit command to exit the program\n\n"""
+        self.assertEqual(string, f.getvalue())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
